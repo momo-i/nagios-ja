616ca3527cec2fc8a150b6b46567b7fb30d163b3
momo-i <webmaster@momo-i.org>
2016-09-17 22:31:01 +0900

    test

--- cgi/avail.c
+++ cgi/avail.c
@@ -3960,7 +3960,11 @@ void display_host_availability(void) {
 		printf("<p align='center'>\n");
 		printf("<a href='%s?host=%s", TRENDS_CGI, url_encode(host_name));
 		printf("&t1=%lu&t2=%lu&includesoftstates=%s&assumestateretention=%s&assumeinitialstates=%s&assumestatesduringnotrunning=%s&initialassumedhoststate=%d&backtrack=%d'>", t1, t2, (include_soft_states == TRUE) ? "yes" : "no", (assume_state_retention == TRUE) ? "yes" : "no", (assume_initial_states == TRUE) ? "yes" : "no", (assume_states_during_notrunning == TRUE) ? "yes" : "no", initial_assumed_host_state, backtrack_archives);
+#ifdef LEGACY_GRAPHICAL_CGIS
 		printf("<img src='%s?createimage&smallimage&host=%s", TRENDS_CGI, url_encode(host_name));
+#else
+		printf("<img src='%s?createimage&smallimage&host=%s", LEGACY_TRENDS_CGI, url_encode(host_name));
+#endif
 		printf("&t1=%lu&t2=%lu&includesoftstates=%s&assumestateretention=%s&assumeinitialstates=%s&assumestatesduringnotrunning=%s&initialassumedhoststate=%d&backtrack=%d' border=1 alt='ホスト状態の傾向' title='ホスト状態の傾向' width='500' height='20'>", t1, t2, (include_soft_states == TRUE) ? "yes" : "no", (assume_state_retention == TRUE) ? "yes" : "no", (assume_initial_states == TRUE) ? "yes" : "no", (assume_states_during_notrunning == TRUE) ? "yes" : "no", initial_assumed_host_state, backtrack_archives);
 		printf("</a><br>\n");
 		printf("</p>\n");
@@ -4444,7 +4448,11 @@ void display_service_availability(void) {
 		printf("<p align='center'>\n");
 		printf("<a href='%s?host=%s", TRENDS_CGI, url_encode(host_name));
 		printf("&service=%s&t1=%lu&t2=%lu&includesoftstates=%s&assumestateretention=%s&assumeinitialstates=%s&assumestatesduringnotrunning=%s&initialassumedservicestate=%d&backtrack=%d'>", url_encode(svc_description), t1, t2, (include_soft_states == TRUE) ? "yes" : "no", (assume_state_retention == TRUE) ? "yes" : "no", (assume_initial_states == TRUE) ? "yes" : "no", (assume_states_during_notrunning == TRUE) ? "yes" : "no", initial_assumed_service_state, backtrack_archives);
+#ifdef LEGACY_GRAPHICAL_CGIS
 		printf("<img src='%s?createimage&smallimage&host=%s", TRENDS_CGI, url_encode(host_name));
+#else
+		printf("<img src='%s?createimage&smallimage&host=%s", LEGACY_TRENDS_CGI, url_encode(host_name));
+#endif
 		printf("&service=%s&t1=%lu&t2=%lu&includesoftstates=%s&assumestateretention=%s&assumeinitialstates=%s&assumestatesduringnotrunning=%s&initialassumedservicestate=%d&backtrack=%d' border=1 alt='サービス状態の傾向' title='サービス状態の傾向' width='500' height='20'>", url_encode(svc_description), t1, t2, (include_soft_states == TRUE) ? "yes" : "no", (assume_state_retention == TRUE) ? "yes" : "no", (assume_initial_states == TRUE) ? "yes" : "no", (assume_states_during_notrunning == TRUE) ? "yes" : "no", initial_assumed_service_state, backtrack_archives);
 		printf("</a><br>\n");
 		printf("</p>\n");
@@ -4722,6 +4730,8 @@ void service_report_url(const char *hn, const char *sd, const char *label) {
 	printf("&showscheduleddowntime=%s", (show_scheduled_downtime == TRUE) ? "yes" : "no");
 	if(current_timeperiod != NULL)
 		printf("&rpttimeperiod=%s", url_encode(current_timeperiod->name));
+	if(include_soft_states == TRUE)
+		printf("&includesoftstates=%s", (include_soft_states == TRUE) ? "yes" : "no");
 	printf("'>%s</a>", label);
 
 	return;
